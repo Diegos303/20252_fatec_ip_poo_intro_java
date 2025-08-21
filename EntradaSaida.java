b18a5c106632038add268a637dda3d2fb0a235e3
Diego <diego.melo4@fatec.sp.gov.br>
2025-08-21 17:54:47 -0300

    learn(ap002-java): faz entrada e saida
    -utilizar interface grafica
    -ainda nao fizemos na linha de comando

---
+++ EntradaSaida.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+public class EntradaSaida{
+  static public void main (String [] args) {
+    //float(4bytes), double(8bytes)
+    //classes empacotadoras(wrappers)
+    
+    double n1, n2;
+    n1 = Double.parseDouble(JOptionPane.showInputDialog("digite um valor"));
+    n2 = Double.parseDouble(JOptionPane.showInputDialog("Digite outro valor"));
+
+    double soma = n1 + n2;
+    JOptionPane.showMessageDialog(null, soma);
+
+    
+  }
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -3,4 +3,7 @@ public class HelloWorld{
     System.out.println("Hello, World");
   }
 }
+
+
+
  
